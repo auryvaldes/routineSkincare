2b7a4853df94f4b214075ccb068418f8e67ad6ab
auryvaldes <47179470+auryvaldes@users.noreply.github.com>
2021-06-23 18:02:35 -0300

    Se visualiza la lista de reoutines

--- src/component/RoutineList.js
+++ src/component/RoutineList.js
@@ -4,13 +4,15 @@ import {View, Text} from 'react-native';
 
 
 class RoutineList extends Component{
+    state = { reoutineList:[] };
+
     componentDidMount() {
-        axios.get('https://my-json-server.typicode.com/auryvaldes/routineSkincare/list')
-        .then((response) => {
-            console.log(response);
-        })
+        axios.get('https://my-json-server.typicode.com/auryvaldes/routineSkincare/list').then((response) => {
+            this.setState({ routineList: response.data});
+        });
     } 
     render() {
+        console.log(this.state);
         return(
             <View>
                 <Text>Routine List</Text>
