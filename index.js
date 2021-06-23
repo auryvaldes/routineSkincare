6a0377d810486768ea011d64c300c3631c829e6a
auryvaldes <47179470+auryvaldes@users.noreply.github.com>
2021-06-23 17:43:42 -0300

    Creacion del RoutineList

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ import React from 'react';
 import {AppRegistry, View} from 'react-native';
 
 import Header from './src/component/Header';
+import RoutineList from './src/component/RoutineList';
 
 const App = () => {
-    return <Header/>
+    return (
+        <View>
+            <Header/>
+            <RoutineList/>
+        </View>
+    ); 
 }
 
 AppRegistry.registerComponent('skincare', () => App);
---
+++ src/component/RoutineList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const RoutineList = () => {
+    return(
+        <View>
+            <Text>Routine List</Text>
+        </View>
+    );
+}
+
+export default RoutineList;
