eb03456090e55e814bde47203b1e665ae74fb3cc
auryvaldes <47179470+auryvaldes@users.noreply.github.com>
2021-06-23 17:58:45 -0300

    Se visualiza el response

--- src/component/RoutineList.js
+++ src/component/RoutineList.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import axios from 'axios';
+import React, {Component} from 'react';
 import {View, Text} from 'react-native';
 
-const RoutineList = () => {
-    return(
-        <View>
-            <Text>Routine List</Text>
-        </View>
-    );
+
+class RoutineList extends Component{
+    componentDidMount() {
+        axios.get('https://my-json-server.typicode.com/auryvaldes/routineSkincare/list')
+        .then((response) => {
+            console.log(response);
+        })
+    } 
+    render() {
+        return(
+            <View>
+                <Text>Routine List</Text>
+            </View>
+        );
+    }
 }
 
 export default RoutineList;
