b1d5307f5281b5fb4fcf1d4919b54c7d6ef47347
auryvaldes <47179470+auryvaldes@users.noreply.github.com>
2021-06-23 19:02:09 -0300

    ya veo los name

--- src/component/RoutineList.js
+++ src/component/RoutineList.js
@@ -4,20 +4,25 @@ import {View, Text} from 'react-native';
 
 
 class RoutineList extends Component{
-    state = { reoutineList:[] };
+    state = { routineList:[] };
 
     componentDidMount() {
         axios.get('https://my-json-server.typicode.com/auryvaldes/routineSkincare/list').then((response) => {
             this.setState({ routineList: response.data});
         });
     } 
+
+    renderList = () => {
+        return this.state.routineList.map((skin) => {
+            return <Text>{skin.routine[0].name}</Text>
+        })
+
+    }
+
+
     render() {
         console.log(this.state);
-        return(
-            <View>
-                <Text>Routine List</Text>
-            </View>
-        );
+        return <View>{this.renderList()}</View>
     }
 }
 
